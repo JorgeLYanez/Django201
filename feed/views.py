e6e668f6deb1380bbdccf7e074f7859669ce4cd7
Jorge <jorge@MacBook-Air-de-Jorge.local>
2024-12-10 16:54:15 -0700

    10-12

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Post
 
-# Create your views here.
+class HomePage(ListView):
+    http_method_names=["get"]
+    template_name="feed/homepage.html"
+    model = Post
+    context_object_name = "posts"
+    queryset= Post.objects.all().order_by('-id')[0:30]
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "feed/detail.html"
+    model= Post
+    context_object_name = "post"
+
